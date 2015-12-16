e03ce7699ee2103bdf65c0507b38f2874dd99bfc
christopher snow <chsnow123@gmail.com>
2015-12-16 13:50:51 +0000

    added test cases

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# cloudant-java-query-examples
+A few test cases for Cloudant Java
+--
+
+ 1. Add your Cloudant credentials to cloudant.properties.
+ 2. Create a database with the following document:
+
+```
+{
+    "_id": "68",
+    "name": "Peanut butter",
+    "title": "Peanut butter is the name",
+    "description": "Peanut butter is the description",
+    "prices": [
+      {
+        "storeName": "Morrisons",
+        "price": 97
+      },
+      {
+        "storeName": "Saintburrys",
+        "price": 72
+      },
+      {
+        "storeName": "Tesco",
+        "price": 79
+      }
+    ],
+    "rev": null
+}
+```
+
+ 3. Create a view:
+ 
+ ```
+ {
+  "_id": "_design/ddoc",
+  "views": {
+    "find-by-product-name": {
+      "map": "function (doc) {
+        if (doc.name) {
+            emit(doc.name, null);
+        }
+      }"
+    },
+    "find-by-store-name": {
+      "map": "function (doc) {
+        var shop, price, storename,storeprice;
+        if (doc.name && doc.prices) {
+           for (shop in doc.prices) {
+              storename = doc.prices[shop].storeName;
+              storeprice = doc.prices[shop].price;
+              emit(storename, storeprice);
+           }
+        }
+      }"
+    }
+  }
+}
+```
+
+
+
+
+
+
---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,115 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.cloudant.client.api.ClientBuilder;
+import com.cloudant.client.api.CloudantClient;
+import com.cloudant.client.api.Database;
+import com.cloudant.client.api.views.Key;
+import com.cloudant.client.api.views.ViewResponse;
+import com.cloudant.client.api.views.ViewResponse.Row;
+
+import static org.junit.Assert.*;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
+
+
+public class ExampleTest {
+	
+	CloudantClient client;
+	Database db;
+		
+	@Before
+	public void setup() throws FileNotFoundException, IOException {
+		
+		Properties props = new Properties();
+		props.load(new FileInputStream("cloudant.properties"));
+		
+		client = ClientBuilder
+					.account(props.getProperty("cloudant_account"))
+		            .username(props.getProperty("cloudant_username"))
+		            .password(props.getProperty("cloudant_password"))
+		            .build();
+		
+		db = client.database(props.getProperty("cloudant_database"), false);
+	}
+	
+	@After
+	public void tearDown() {
+		client.shutdown();
+	}
+
+	@Test
+	public void testFindAllProducts() throws IOException {
+		
+		 List<String> list = db.getViewRequestBuilder("ddoc","find-by-product-name")
+				    .newRequest(Key.Type.STRING, Object.class)
+				 	  .limit(10)
+				 	  .includeDocs(true)
+				 	  .build()
+				 	  .getResponse()
+				 	  .getKeys();
+		 
+		 assertEquals(1, list.size());
+	}
+
+	
+	@Test
+	public void testFindByProductName() throws IOException {
+		
+		 List<String> list = db.getViewRequestBuilder("ddoc","find-by-product-name")
+				    .newRequest(Key.Type.STRING, Object.class)
+				      .keys("Peanut butter")
+				 	  .limit(10)
+				 	  .includeDocs(true)
+				 	  .build()
+				 	  .getResponse()
+				 	  .getKeys();
+		 
+		 String[] expected = { "Peanut butter" };
+		 assertArrayEquals(expected, list.toArray());
+	}
+	
+	@Test
+	public void testFindByProductNameNoMatch() throws IOException {
+		
+		 List<String> list = db.getViewRequestBuilder("ddoc","find-by-product-name")
+				    .newRequest(Key.Type.STRING, Object.class)
+				      .keys("XXXXX")
+				 	  .limit(10)
+				 	  .includeDocs(true)
+				 	  .build()
+				 	  .getResponse()
+				 	  .getKeys();
+		 
+		 assertEquals(0, list.size());
+	}
+	
+	@Test
+	public void testFindByStoreName() throws IOException {
+	
+		ViewResponse<String, Integer> response = db.getViewRequestBuilder("ddoc","find-by-store-name")
+				    .newRequest(Key.Type.STRING, Integer.class)
+				 	  .startKey("Mo")
+				 	  .endKey("T")
+				 	  .inclusiveEnd(true)
+				 	  .limit(10)
+				 	  .includeDocs(true)
+				 	  .descending(false)
+				 	  .build()
+				 	  .getResponse();
+		
+		List<Row<String, Integer>> rows = response.getRows();
+		 
+		assertEquals("Morrisons", 			rows.get(0).getKey());
+		assertEquals(Integer.valueOf(97), 	rows.get(0).getValue());
+		
+		assertEquals("Saintburrys", 		rows.get(1).getKey());
+		assertEquals(Integer.valueOf(72), 	rows.get(1).getValue());
+	}
+	
+}
